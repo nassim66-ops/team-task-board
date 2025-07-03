c46c5b303429e4eac2a08f5110783d37d4cb89b8
Nassim Sleem <nassim@aktek.io>
2025-07-03 21:41:27 +0300

    cleaning the code

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,6 +1,5 @@
 import { initTRPC } from '@trpc/server';
 import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../types/supabase';
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error('Supabase environment variables not set!');
